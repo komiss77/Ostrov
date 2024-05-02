6ff8f22228edfcb5fc056185035f5f2301ccb88d
Romindous <Romindous@gmail.com>
2024-05-02 18:16:35 -0400

    1.20.6 broken shadow

--- src/ru/komiss77/utils/InvStatus.java
+++ src/ru/komiss77/utils/InvStatus.java
@@ -2,25 +2,9 @@ package ru.komiss77.utils;
 
 import org.bukkit.entity.Player;
 
-public class InvStatus {
-
-    private boolean isArmor;
-    private Player player;
-
-    public InvStatus(boolean isArmor, Player p) {
-        this.isArmor = isArmor;
-        this.player = p;
-    }
-
-    public boolean isArmor() {
-        return this.isArmor;
-    }
+public record InvStatus(boolean isArmor, Player player) {
 
     public boolean isOnline() {
         return this.player != null && this.player.isOnline();
-    }
-
-    public Player getPlayer() {
-        return this.player;
     }
 }
