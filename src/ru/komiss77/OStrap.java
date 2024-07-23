32d8d2dc2be1a17b486ec6573e8b88606df775ac
BuildTools <unconfigured@null.spigotmc.org>
2024-07-23 11:50:43 +0300

    first 1.21 remove shadow

---
+++ src/ru/komiss77/OStrap.java
@@ -0,0 +1,70 @@
+package ru.komiss77;
+
+
+import java.util.Collection;
+import java.util.Locale;
+
+import io.papermc.paper.plugin.bootstrap.BootstrapContext;
+import io.papermc.paper.plugin.bootstrap.PluginBootstrap;
+import io.papermc.paper.plugin.lifecycle.event.LifecycleEventManager;
+import io.papermc.paper.registry.RegistryAccess;
+import io.papermc.paper.registry.RegistryKey;
+import io.papermc.paper.registry.TypedKey;
+import io.papermc.paper.registry.data.EnchantmentRegistryEntry;
+import io.papermc.paper.registry.event.RegistryEvents;
+import io.papermc.paper.registry.event.WritableRegistry;
+import io.papermc.paper.registry.set.RegistryKeySet;
+import io.papermc.paper.registry.set.RegistrySet;
+import net.kyori.adventure.key.Key;
+import org.bukkit.Keyed;
+import org.bukkit.NamespacedKey;
+import org.bukkit.Registry;
+import org.bukkit.enchantments.Enchantment;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import ru.komiss77.modules.enchants.CustomEnchant;
+import ru.komiss77.modules.enchants.EnchantManager;
+import ru.komiss77.utils.TCUtils;
+
+public class OStrap implements PluginBootstrap {
+
+    public static final String space = "ostrov";
+
+    public static NamespacedKey key(final String key) {
+        return new NamespacedKey(space, key.toLowerCase(Locale.ROOT));
+    }
+
+    @Override
+    public void bootstrap(@NotNull BootstrapContext cntx) {
+        final @NotNull LifecycleEventManager<BootstrapContext> mgr = cntx.getLifecycleManager();
+        mgr.registerEventHandler(RegistryEvents.ENCHANTMENT
+                .freeze().newHandler(e -> {
+                    EnchantManager.init();
+                    final @NonNull WritableRegistry<Enchantment, EnchantmentRegistryEntry.Builder> rg = e.registry();
+                    for (final CustomEnchant ce : CustomEnchant.VALUES.values()) {
+                        rg.register(TypedKey.create(RegistryKey.ENCHANTMENT, ce.getKey()),
+                                b -> b.description(TCUtils.form(ce.name()))
+                                        .primaryItems(ce.isCommon() ? ce.targets().regSet() : RegistrySet.keySet(RegistryKey.ITEM))
+                                        .supportedItems(ce.isCommon() ? RegistrySet.keySet(RegistryKey.ITEM) : ce.targets().regSet())
+                                        .anvilCost(ce.anvilCost())
+                                        .maxLevel(ce.maxLevel())
+                                        .weight(ce.weight())
+                                        .exclusiveWith(ce.conflicts())
+                                        .minimumCost(ce.minCost())
+                                        .maximumCost(ce.maxCost())
+                                        .activeSlots(ce.slots()));
+                    }
+                }));
+    }
+
+    public static <T extends Keyed> RegistryKeySet<T> regSetOf(final RegistryKey<T> reg, final Collection<Key> keys) {
+        return RegistrySet.keySet(reg, keys.stream().map(k -> TypedKey.create(reg, k)).toList());
+    }
+
+    @Nullable
+    public static <E extends Keyed> E retrieve(final RegistryKey<E> reg, final Key key) {
+        final Registry<E> enchantmentRegistry = RegistryAccess.registryAccess().getRegistry(reg);
+        return enchantmentRegistry.get(TypedKey.create(reg, Key.key("minecraft:sharpness")));
+    }
+}
---
+++ src/ru/komiss77/modules/items/CustomMats.java
@@ -0,0 +1,112 @@
+package ru.komiss77.modules.items;
+
+import org.bukkit.Keyed;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.ProjectileLaunchEvent;
+import org.bukkit.event.player.PlayerEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import ru.komiss77.Config;
+import ru.komiss77.Ostrov;
+import ru.komiss77.utils.ItemUtils;
+import ru.komiss77.utils.OstrovConfig;
+
+import javax.annotation.Nullable;
+import java.util.*;
+
+public abstract class CustomMats implements Keyed {
+
+    private static final String CMD = "cmd";
+    private static final String ITS = "its";
+    private static final String SEP = "=";
+    private static final String CON_NAME = "items.yml";
+    private static final HashMap<Integer, CustomMats> VALUES = new HashMap<>();
+
+    public final Integer cmd;
+    private final EnumMap<Material, ItemStack> mits = new EnumMap<>(Material.class);
+
+    protected final NamespacedKey key;
+
+    protected CustomMats(final Integer cmd, final ItemStack... its) {
+        this.cmd = cmd;
+        this.key = new NamespacedKey(Ostrov.instance, this.getClass().getSimpleName());
+        final OstrovConfig irc = Config.manager.getNewConfig(CON_NAME);
+        final Collection<String> itls = irc.getStringList(key().value());
+        if (itls.isEmpty()) {
+            for (final ItemStack it : its) {
+                if (ItemUtils.isBlank(it, false)) continue;
+                mits.put(it.getType(), it);
+            }
+            irc.set(key().value(), Arrays.stream(its).map(it -> ItemUtils.toString(it, SEP)).toList());
+            irc.saveConfig();
+        } else {
+            for (final String is : itls) {
+                final ItemStack it = ItemUtils.parseItem(is, SEP);
+                mits.put(it.getType(), it);
+            }
+        }
+
+        VALUES.put(cmd, this);
+    }
+
+    public static CustomMats[] values() {
+        return VALUES.values().toArray(new CustomMats[0]);
+    }
+
+    public @Nullable ItemStack getItem(final Material mt) {
+        return mits.get(mt);
+    }
+
+    public static CustomMats getCstmItm(final ItemStack it) {
+        return it.hasItemMeta() ? getCstmItm(it.getItemMeta()) : null;
+    }
+
+    public static CustomMats getCstmItm(final ItemMeta im) {
+        return im.hasCustomModelData() ? getCstmItm(im.getCustomModelData()) : null;
+    }
+
+    public static CustomMats getCstmItm(final Integer cmd) {
+        return VALUES.get(cmd);
+    }
+
+    protected abstract void onAttack(final EquipmentSlot[] es, final EntityDamageByEntityEvent e);
+
+    protected abstract void onDefense(final EquipmentSlot[] es, final EntityDamageEvent e);
+
+    protected abstract void onShoot(final EquipmentSlot[] es, final ProjectileLaunchEvent e);
+
+    protected abstract void onInteract(final EquipmentSlot[] es, final PlayerInteractEvent e);
+
+    protected abstract void onBreak(final EquipmentSlot[] es, final BlockBreakEvent e);
+
+    protected abstract void onPlace(final EquipmentSlot[] es, final BlockPlaceEvent e);
+
+    protected abstract void onExtra(final EquipmentSlot[] es, final PlayerEvent e);
+
+    @Override
+    public NamespacedKey getKey() {
+        return key;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        return o instanceof CustomMats
+                && Objects.equals(((CustomMats) o).cmd, cmd);
+    }
+
+    @Override
+    public int hashCode() {
+        return cmd == null ? 0 : cmd;
+    }
+
+    protected static void load() {
+        VALUES.clear();
+    }
+}
