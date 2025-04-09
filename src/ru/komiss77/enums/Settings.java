fc2966c8f840a78e8f3aa567599617e42178c197
Romindous <Romindous@gmail.com>
2025-04-09 11:28:40 -0400

    Вернул Прокси вещи, починим Deprecated как версия выйдет

--- src/ru/komiss77/enums/Settings.java
+++ src/ru/komiss77/enums/Settings.java
@@ -16,6 +16,31 @@ public enum Settings {
     Fr_SendExitMsgDeny(4, 100, "§6Отправление оповещений о выходе", Arrays.asList("<mithril>Отправлять друзьям оповещение", "<mithril>когда вы отключетесь?")),
     Fr_RecieveSwitchMsgDeny(5, 7, "§6Оповещения о Переходах", Arrays.asList("<mithril>Получать оповещения", "<mithril>когда друзья меняют сервер?")),
     Fr_SendSwitchMsgDeny(6, 8, "§6Отправка Переходов", Arrays.asList("<mithril>Оповещять друзей когда", "<mithril>ты меняешь режим?")),
+    Fr_HideOnline(7, 1, "§3Режим 'Невидимка'", Arrays.asList("<mithril>показывать друзьям", "<mithril>ваше присутствие на сервере?")),
+    Fr_MsgDeny(8, 3, "§3Личные Сообщения", Arrays.asList("<mithril>Получать личные сообщения", "<mithril>от друзей? Так же работает", "<mithril>когда ты оффлайн!")),
+
+    @Deprecated //зачем разделять обычные и оффлайн?
+    Fr_MsgOfflineDeny(9, 100, "§3Оффлайн сообщения", Arrays.asList("<mithril>Разрешить друзьям ",
+        "<mithril>оставлять Вам сообщения", "<mithril>когда Вы не на сервере?", "<mithril>(Вы их сможете прочитать", "<mithril>при следующем входе)")),// - отделььная графа
+    @Deprecated
+    Fr_TeleportDeny(10, 100, "§eМаячёк", Arrays.asList("<mithril>Разрешить друзьям", "<mithril>отправлять Вам запросы", "<mithril>на телепорт?")),
+    @Deprecated
+    Fr_InviteDeny(11, 100, "§6Открытость", Arrays.asList("<mithril>Получать предложения дружить", "<mithril>от других игроков?")),
+
+    @Deprecated
+    Fr_ShowFriendDeny(12, 100, "§6Видеть друзей в лобби", Arrays.asList("")),
+    @Deprecated
+    Fr_ShowPartyDeny(13, 100, "§6Видеть команду в лобби", Arrays.asList("")),
+    @Deprecated
+    Fr_ShowOtherDeny(14, 100, "§6Видеть остальных в лобби", Arrays.asList("")),
+    @Deprecated
+    Party_LeaderTrackDeny(15, 100, "§eОтслеживать лидера", Arrays.asList("<mithril>Получать уведомления,", "<mithril>когда лидер меняет сервер?")),
+    @Deprecated
+    Party_LeaderFollowDeny(16, 100, "§eСледовать за лидером", Arrays.asList("<mithril>Телепортировать Вас к лидеру", "<mithril>когда он меняет сервер?", "<mithril>(полезно для командных игр)")),
+    @Deprecated
+    Party_InviteFriendsDeny(17, 100, "§eОткрытость друзьям", Arrays.asList("<mithril>Получать предложения", "<mithril>вступить в команду от друзей?")),
+    @Deprecated
+    Party_InviteOtherDeny(18, 100, "§eОткрытость остальным", Arrays.asList("<mithril>Получать предложения", "<mithril>вступить в команду от остальных?")),
 
     HideOnline(7, 1, "§3Режим 'Невидимка'", Arrays.asList("<mithril>показывать друзьям", "<mithril>ваше присутствие на сервере?")),
     MsgDeny(8, 3, "§3Личные Сообщения", Arrays.asList("<mithril>Получать личные сообщения", "<mithril>от друзей? Так же работает", "<mithril>когда ты оффлайн!")),
@@ -27,7 +52,7 @@ public enum Settings {
     HideNonFriends(12, 4, "§9Видимость Игроков §8(лобби)", Arrays.asList("<mithril>Видеть игроков, которые", "<mithril>не у тебя в друзьях?")),
 
     @Deprecated //переделал
-    ShowOtherDeny(13, 8, "§6Видеть остальных в лобби", Arrays.asList("")),
+    ShowOtherDeny(13, 100, "§6Видеть остальных в лобби", Arrays.asList("")),
 
     HideNonParty(14, 4, "§9Видеть только комманду §8(лобби)", Arrays.asList("<mithril>Видеть игроков, которые", "<mithril>не у тебя в комманде?")),
     LeaderTrackDeny(15, 0, "§eОтслеживать лидера", Arrays.asList("<mithril>Получать уведомления,", "<mithril>когда лидер меняет сервер?")),
@@ -36,7 +61,7 @@ public enum Settings {
     InviteOthersDeny(18, 3, "§eОткрытость остальным", Arrays.asList("<mithril>Получать предложения", "<mithril>вступить в команду от остальных?")),
 
     @Deprecated //это нужно только гостям + это ломает квесты на серверах где ОНИ ИСПОЛЬЗУЮТСЯ
-    JustGame(19, 0, "§8Играть без квестов", Arrays.asList("<mithril>Играть без всяких квестов")),
+    JustGame(19, 100, "§8Играть без квестов", Arrays.asList("<mithril>Играть без всяких квестов")),
     ;
 
     public final int tag;
