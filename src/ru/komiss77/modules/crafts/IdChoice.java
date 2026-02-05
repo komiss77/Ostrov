4f03d8fd5815a68244236ebc0a1fc62c6f470cee
Romindous <romindous@gmail.com>
2026-02-05 13:47:25 -0500

    Какой то модуль ссылается на классы которых не существует (Trade)

--- src/ru/komiss77/modules/crafts/IdChoice.java
+++ src/ru/komiss77/modules/crafts/IdChoice.java
@@ -1,18 +1,21 @@
 package ru.komiss77.modules.crafts;
 
 import javax.annotation.Nullable;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ItemType;
 import org.bukkit.inventory.RecipeChoice;
-import org.bukkit.inventory.RecipeChoice.MaterialChoice;
 import org.bukkit.persistence.PersistentDataType;
 import ru.komiss77.modules.items.ItemGroup;
 import ru.komiss77.utils.ItemUtil;
 
-public class IdChoice extends MaterialChoice {
+public class IdChoice implements RecipeChoice {
 
     private final String id;
+    private final List<ItemType> choices;
 
     public static RecipeChoice of(final @Nullable ItemStack it) {
         if (ItemUtil.isBlank(it, false)) return RecipeChoice.empty();
@@ -20,50 +23,57 @@ public class IdChoice extends MaterialChoice {
         return new IdChoice(id, it.getType());
     }
 
-    public IdChoice(final @Nullable String id, final Material... mt) {
-        super(mt);
+    public IdChoice(final @Nullable String id, final List<ItemType> choices) {
         this.id = id;
+        this.choices = choices;
         //Bukkit.broadcast(Component.text("mt-" + mt[0].toString() + ", cmd-" + cmd));
     }
 
-    public IdChoice(final MaterialChoice mtc) {
-        super(mtc.getChoices());
-        this.id = null;
+    public IdChoice(final @Nullable String id, final ItemType... mt) {
+        this.id = id;
+        this.choices = Arrays.asList(mt);
+        //Bukkit.broadcast(Component.text("mt-" + mt[0].toString() + ", cmd-" + cmd));
+    }
+
+    @Deprecated
+    public IdChoice(final @Nullable String id, final Material... mt) {
+        this.id = id;
+        this.choices = Arrays.stream(mt).map(Material::asItemType).toList();
+        //Bukkit.broadcast(Component.text("mt-" + mt[0].toString() + ", cmd-" + cmd));
     }
 
     private static final Material[] emt = {null};
 
     @Override
     public IdChoice clone() {
-        return new IdChoice(id, getChoices().toArray(emt));
+        return new IdChoice(id, choices);
     }
 
     @Override
     public ItemStack getItemStack() {
-        final Material mt = getChoices().getFirst();
+        final ItemType mt = choices.getFirst();
         final ItemGroup cmts = ItemGroup.get(id);
-        final ItemStack ci = cmts == null ? null : cmts.item(mt.asItemType());
+        final ItemStack ci = cmts == null ? null : cmts.item(mt);
         if (ci != null) return ci.asOne();
-        final ItemStack it = new ItemStack(mt);
+        final ItemStack it = mt.createItemStack();
         return ItemUtil.isBlank(it, false) ? ItemUtil.air : it;
     }
 
     @Override
     public boolean test(final ItemStack it) {
-        if (it == null) return getChoices().contains(Material.AIR);
-        if (!getChoices().contains(it.getType())) return false;
+        if (it == null) return choices.contains(ItemType.AIR);
+        if (!choices.contains(it.getType())) return false;
         return Objects.equals(id, it.getPersistentDataContainer()
             .get(ItemGroup.KEY, PersistentDataType.STRING));
     }
 
     @Override
     public boolean equals(final Object o) {
-        if (o instanceof MaterialChoice) {
-            for (final Material mt : ((MaterialChoice) o).getChoices()) {
-                if (!getChoices().contains(mt)) return false;
+        if (o instanceof final IdChoice oic) {
+            for (final ItemType it : oic.choices) {
+                if (!choices.contains(it)) return false;
             }
-            return o instanceof IdChoice ?
-                Objects.equals(((IdChoice) o).id, id) : id == null;
+            return Objects.equals(oic.id, id);
         }
         return false;
     }
